4b66b4f21247b96d00dc7c3cb871db0b6c60f4c6
alexfilenko <filenkoav@gmail.com>
2016-08-09 19:28:36 +0300

    model question create

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ApplicationRecord
+  belongs_to :user
+  has_many :answers, dependent: :destroy
+  
+  validates :title, :body, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :user_id }
+  end
 end
