6a7132748fda820b8ca9e03be3a6d5136fa9b32f
alexfilenko <filenkoav@gmail.com>
2016-08-11 12:51:42 +0300

    config.include FactoryGirl::Syntax::Methods

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
-  	it 'create question' do
-  	  question1 = FactoryGirl.create(:question)
-  	  question2 = FactoryGirl.create(:question)
-
+	before do
+  	  @questions = FactoryGirl.create_list(:question, 2)
   	  get :index
+	end
 
-  	  expect(assigns(:questions)).to match_array([question1,question2])
+  	it 'create question' do
+  	  expect(assigns(:questions)).to match_array(@questions)
   	end
 
   	it 'renders index view' do
-  	  get :index
   	  expect(response).to render_template :index
 end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -29,6 +30,7 @@ require 'factory_girl_rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
