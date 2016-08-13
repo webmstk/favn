ccc31e4adadf6324637db3002ae9772d0e55a399
alexfilenko <filenkoav@gmail.com>
2016-08-13 11:29:46 +0300

    test answers controller
    create new and create met

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+	before_action :load_question, only: [:new, :create]
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @Answer = Answer.new(answer_params)
+    
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+	let(:answer) { create(:answer) }
+
+  describe 'GET #new' do
+    before { get :new }
+    
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+    
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 
 end
